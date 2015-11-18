5e25418695a228bc6b6876d615b12107f29c1a48
Subrosian <subrosian@hotmail.com>
2015-11-17 17:26:23 -0800

    Trapdoor commit

--- MadScienceLab/MadScienceLab/Trapdoor.cs
+++ MadScienceLab/MadScienceLab/Trapdoor.cs
@@ -10,8 +10,12 @@ namespace MadScienceLab
     class Trapdoor:Door
     {
         public Trapdoor(int column, int row, Boolean isOpen):base(column, row, isOpen) {
+            int verticalOffset = -GameConstants.SINGLE_CELL_SIZE / 2 + 5;
+            Translate(this.Position.X, this.Position.Y - verticalOffset, this.Position.Z);
             Rotate(0, 0, 90f);
-            //UpdateBoundingBox(base.Model, Matrix.CreateTranslation(base.Position), false, false);
+            UpdateBoundingBox(GameplayScreen._models["BasicBlock"], /*Matrix.CreateRotationZ(90f) * */Matrix.CreateTranslation(base.Position), false, false);
+            HitboxHeightOffset = verticalOffset;
+            //HitboxHeight /= 2;
         }
     }
 }
